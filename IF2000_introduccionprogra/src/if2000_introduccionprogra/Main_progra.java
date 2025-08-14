66c930849e17e813b402b2e30573e2544dcadd44
carranzaelberth505-del <carranzaelberth505@gmail.com>
2025-08-14 11:22:14 -0600

    Mi primer commit en java

---
+++ IF2000_introduccionprogra/src/if2000_introduccionprogra/Main_progra.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package if2000_introduccionprogra;
+
+/**
+ *
+ * @author Lenovo X1
+ */
+public class Main_progra {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+     //data types
+     String name= " La cabra";
+     int age= 40;
+     
+     System.out.println("Hola mundo");
+     System.out.println("Name" + name);
+     System.out.println("Age " + age);
+     System.out.println("Adios mundo");
+     /*tipos de datos numericos 
+     
+     int, long, short, float, double, numeric, money, decimal
+     
+     Manejo de fechas:
+     Date
+     
+     Cadenas de caracteres:
+     String, cahr
+     
+     Obejtos:
+     Object
+     
+     Data Structures:
+     
+     Arrays or Vectors: int[]x;
+                        int[]y;
+     
+     True/False
+     boolean 
+  
+     */
+     
+     
+     
+    
+    
+    }
+    
+}
